aabb362ef5abab2d0d3806feca616942d5760311
JR Mishra <jrmnitdgp@gmail.com>
2020-09-26 15:42:26 +0530

    [JR Mishra] Add To check if employee is present or not

--- Program.cs
+++ Program.cs
@@ -8,6 +8,23 @@ namespace EmployeeWages
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Console.WriteLine("=============================================");
+
+            bool attendance;
+            attendance = CheckPresentOrAbsent();
+            if (attendance)
+                Console.WriteLine("Employee is Present");
+            return; 
+        }
+
+        static bool CheckPresentOrAbsent()
+        {
+            Random random = new Random();
+            int IS_PRESENT = 1;
+            int eCheck = random.Next(0, 2);
+            if (eCheck == IS_PRESENT)
+                return true;
+            else
+                return false;
         }
     }
 }
