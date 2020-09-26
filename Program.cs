ded20c75f786fd5624cb443faef00714aa50100c
JR Mishra <jrmnitdgp@gmail.com>
2020-09-26 16:07:40 +0530

    [JR Mishra] Add Calculate Daily Wage

--- Program.cs
+++ Program.cs
@@ -9,10 +9,19 @@ namespace EmployeeWages
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Console.WriteLine("=============================================");
 
+            int FULL_DAY_HOUR = 8;
+            int HALF_DAY_HOUR = 4;
+
             bool attendance;
             attendance = CheckPresentOrAbsent();
             if (attendance)
                 Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+
+            int dailyWage;
+            dailyWage = DailyWage(attendance, FULL_DAY_HOUR);
+            Console.WriteLine("So, Today's Wage is Rs. {0}.00", dailyWage);
             return; 
         }
 
@@ -25,6 +34,15 @@ namespace EmployeeWages
                 return true;
             else
                 return false;
+        }
+
+        static int DailyWage(bool presence, int workHour)
+        {
+            int WAGE_PER_HOUR = 20;
+            int dailyWage = 0;
+            if (presence)
+                dailyWage = WAGE_PER_HOUR * workHour;
+            return dailyWage;
         }
     }
 }
