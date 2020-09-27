95d582d55ac33985e982ea594f5dae1b477d2b75
JR Mishra <jrmnitdgp@gmail.com>
2020-09-27 21:08:28 +0530

    [JR Mishra] Refactor. Daily Wage System Upated to support part time employees

--- Program.cs
+++ Program.cs
@@ -12,36 +12,42 @@ namespace EmployeeWages
             int FULL_DAY_HOUR = 8;
             int HALF_DAY_HOUR = 4;
 
+            int empID;
+            empID = GenerateEmpId();
+
             bool attendance;
             attendance = CheckPresentOrAbsent();
 
-            
+            int dailyWorkHours;
+            bool isFullTime;
+            string ifFullTime;
+            Console.WriteLine("Is the employee with Id No. {0} a full time employee ? ( yes / no )",empID);
+            ifFullTime = Console.ReadLine().ToLower();
+
+            switch (ifFullTime)
+            {
+                case "yes":
+                    isFullTime = true;
+                    dailyWorkHours = FULL_DAY_HOUR;
+                    break;
+                case "no":
+                    isFullTime = false;
+                    dailyWorkHours = HALF_DAY_HOUR;
+                    break;
+                default:
+                    Console.WriteLine("You choosed wrong option\n" +
+                        "The person got assigned as full time employee");
+                    dailyWorkHours = FULL_DAY_HOUR;
+                    isFullTime = true;
+                    break;
+            }
+
+            int dailyWage;
             if (attendance)
             {
-                string isFullTime;
-
                 Console.WriteLine("Employee is Present");
-                Console.WriteLine("Is the above one a full time employee ? ( yes / no )");
-                isFullTime = Console.ReadLine().ToLower();
-
-                int dailyWage;
-                switch (isFullTime)
-                {
-
-                    case "yes":
-                        dailyWage = DailyWage(attendance, FULL_DAY_HOUR);
-                        Console.WriteLine("As a full time employee, his/her");
-                        Console.WriteLine("today's wage is Rs. {0}.00", dailyWage);
-                        break;
-                    case "no":
-                        dailyWage = DailyWage(attendance, HALF_DAY_HOUR);
-                        Console.WriteLine("As a part time employee, his/her");
-                        Console.WriteLine("today's wage is Rs. {0}.00", dailyWage);
-                        break;
-                    default:
-                        Console.WriteLine("You choosed wrong option");
-                        break;
-                }
+                dailyWage = DailyWage(attendance, FULL_DAY_HOUR);
+                Console.WriteLine("today's wage is Rs. {0}.00", dailyWage);
             }
             else
             {
@@ -50,7 +56,7 @@ namespace EmployeeWages
             }
 
             int monthlyWage;
-            monthlyWage = CalculateMonthlyWage();
+            monthlyWage = CalculateMonthlyWage(isFullTime);
             Console.WriteLine("The monthly wage of that employee is " + monthlyWage);
 
             Console.WriteLine("Enter\n" +
@@ -66,23 +72,29 @@ namespace EmployeeWages
                 case 1:
                     Console.WriteLine("Enter Number of days: ");
                     int days = Int32.Parse(Console.ReadLine());
-                    totalWage = DailyWage(true, days * FULL_DAY_HOUR);
+                    totalWage = DailyWage(true, days * dailyWorkHours);
                     Console.WriteLine("Total wage of " + days + " days is " + totalWage);
                     break;
                 case 2:
                     Console.WriteLine("Enter Number of hourss: ");
                     int hours = Int32.Parse(Console.ReadLine());
                     totalWage = DailyWage(true, hours);
-                    Console.WriteLine("Total wage of " + hours + " hourss is " + totalWage);
+                    Console.WriteLine("Total wage of " + hours + " hours is " + totalWage);
                     break;
                 default:
                     break;
             }
             Console.WriteLine("\nThank you for using Employee Wage Computation Program");
-
+            Console.ReadLine();
             return; 
         }
 
+        static int GenerateEmpId()
+        {
+            Random random = new Random();
+            int eId = random.Next(1, 100);
+            return eId;
+        }
         static bool CheckPresentOrAbsent()
         {
             Random random = new Random();
@@ -103,17 +115,20 @@ namespace EmployeeWages
             return dailyWage;
         }
 
-        static int CalculateMonthlyWage()
+        static int CalculateMonthlyWage(bool typeOfEmployee)
         {
             int idx;
-            int monthlyPresence =0, monthlyWage = 0;
+            int monthlyPresence = 0, monthlyWage = 0, dailyHours = 4; ;
+
+            if (typeOfEmployee)
+                dailyHours = 8;
 
             for(idx=0; idx<20; idx++)
             {
                 if (CheckPresentOrAbsent())
                     monthlyPresence++;
             }
-            monthlyWage = monthlyPresence * 8 * 20;
+            monthlyWage = monthlyPresence * dailyHours * 20;
 
             return monthlyWage;
         }
