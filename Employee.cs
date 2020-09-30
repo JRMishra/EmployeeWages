a02f3d568437c6c3ceaa80a27fcad86a1b873043
JR Mishra <jrmnitdgp@gmail.com>
2020-09-30 12:29:21 +0530

    [JR Mishra] Refactor. OOP Concepts Applied and restructured

---
+++ Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class Employee
+    {
+        bool _isPresent;
+        bool _isFullTime;
+
+        public Employee()
+        {
+            this._isPresent = true;
+            this._isFullTime = true;
+        }
+
+        public bool IsPresent {
+            get
+            {
+                Random randomNum = new Random();
+                int num = randomNum.Next(0, 2);
+
+                this._isPresent = (num == 1);
+                return _isPresent;
+            }
+        }
+        public bool IsFullTime {
+            get
+            {
+                Random randomNum = new Random();
+                int num = randomNum.Next(1, 3);
+
+                this._isFullTime = (num == 2);
+                return _isFullTime;
+            }
+        }
+    }
+}
